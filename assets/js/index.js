6b3c5c88b29a044906542442172b42e753d99434
marcoant08 <marcoant008@gmail.com>
2021-04-12 21:18:20 -0300

    lib face-api adicionada

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,32 @@
 const cam = document.getElementById("cam");
 
-navigator.mediaDevices.enumerateDevices().then((devices) => {
-  console.log("Devices:", devices);
-  if (Array.isArray(devices)) {
-    devices.forEach((device) => {
-      if (device.kind === "videoinput") {
-        console.log("Device selected:\n", device);
-        navigator.getUserMedia(
-          {
-            video: {
-              deviceId: device.deviceId,
+const startVideo = () => {
+  navigator.mediaDevices.enumerateDevices().then((devices) => {
+    console.log("Devices:", devices);
+    if (Array.isArray(devices)) {
+      devices.forEach((device) => {
+        if (device.kind === "videoinput") {
+          console.log("Device selected:\n", device);
+          navigator.getUserMedia(
+            {
+              video: {
+                deviceId: device.deviceId,
+              },
             },
-          },
-          (stream) => (cam.srcObject = stream),
-          (error) => console.log("Erro:\n", error)
-        );
-      }
-    });
-  }
-});
+            (stream) => (cam.srcObject = stream),
+            (error) => console.log("Erro:\n", error)
+          );
+        }
+      });
+    }
+  });
+};
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/assets/lib/face-api/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/assets/lib/face-api/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/assets/lib/face-api/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/assets/lib/face-api/models"),
+  faceapi.nets.ageGenderNet.loadFromUri("/assets/lib/face-api/models"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/assets/lib/face-api/models"),
+]).then(startVideo);
